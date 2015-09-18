1e8819d8884df860a62e01e2ab600966d587f139
Sander van Grieken <sander@outrightsolutions.nl>
2015-09-18 18:56:01 +0200

    request captcha-needed status from reddit, so we don't always need to show the captcha

--- src/captchamanager.h
+++ src/captchamanager.h
@@ -6,6 +6,7 @@
 class CaptchaManager : public AbstractManager
 {
     Q_OBJECT
+    Q_PROPERTY(bool captchaNeeded READ captchaNeeded NOTIFY captchaNeededChanged)
     Q_PROPERTY(QUrl imageUrl READ imageUrl NOTIFY imageUrlChanged)
     Q_PROPERTY(bool ready READ ready NOTIFY readyChanged)
     Q_PROPERTY(QString iden READ iden)
@@ -14,22 +15,32 @@ public:
     QUrl imageUrl();
     bool ready();
     QString iden();
+    bool captchaNeeded();
 
     Q_INVOKABLE void request();
 
 signals:
     void error(const QString &errorString);
+    void captchaNeededChanged();
     void imageUrlChanged();
     void readyChanged();
 
 private slots:
     void onRequestFinished(QNetworkReply *reply);
+    void onCaptchaNeededFinished(QNetworkReply *reply);
 
 private:
     APIRequest *m_request;
     bool m_ready;
     QString m_iden;
 
+    /* When Unknown or False, captchaNeeded returns false
+     * when Error or True, captchaNeeded returns true
+     */
+    enum CaptchaNeededState { Unknown, True, False, Error };
+    CaptchaNeededState m_captchaNeededState;
+
+    void requestCaptchaNeeded();
     void abortActiveReply();
 };
 
