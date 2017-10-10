5b41c816de50e17d1dcdcb6617c0ad6aa8e86345
Sander van Grieken <sander@outrightsolutions.nl>
2017-10-10 20:15:49 +0200

    map distinguished attribute

--- src/messagemodel.cpp
+++ src/messagemodel.cpp
@@ -39,6 +39,8 @@ QVariantMap MessageModel::toMessageVariantMap(const MessageObject &m)
     map["context"] = m.context();
     map["isComment"] = m.isComment();
     map["isUnread"] = m.isUnread();
+    map["distinguished"] = m.distinguished();
+
     return map;
 }
 
